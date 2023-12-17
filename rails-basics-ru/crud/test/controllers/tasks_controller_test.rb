a28a86c7cc604826fdf8eed5f2d953871cd28f65
@hexlet/cli <support@hexlet.io>
2023-12-17 14:01:19 +0100

    submit rails-basics-ru/crud

--- rails-basics-ru/crud/test/controllers/tasks_controller_test.rb
+++ rails-basics-ru/crud/test/controllers/tasks_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @task_attributes = {
+      name: Faker::Name.name_with_middle,
+      description: Faker::Lorem.question,
+      status: 'new',
+      creator: Faker::Name.name_with_middle,
+      performer: Faker::Name.name_with_middle,
+      completed: true
+    }
+    @task = Task.create(@task_attributes)
+  end
+
+  test 'should destroy task' do
+    delete task_url(@task)
+
+    assert_redirected_to tasks_url
+  end
 end
